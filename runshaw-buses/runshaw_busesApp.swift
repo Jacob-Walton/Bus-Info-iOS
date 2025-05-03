9c82ac85b4719e5557bd6213864d77bbe865d5ad
Jacob-Walton <jacob-walton@konpeki.co.uk>
2025-05-03 10:58:20 +0100

    Initial Commit

---
+++ runshaw-buses/runshaw_busesApp.swift
@@ -0,0 +1,17 @@
+//
+//  runshaw_busesApp.swift
+//  runshaw-buses
+//
+//  Created by Jacob on 03/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct runshaw_busesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ runshaw-busesTests/runshaw_busesTests.swift
@@ -0,0 +1,17 @@
+//
+//  runshaw_busesTests.swift
+//  runshaw-busesTests
+//
+//  Created by Jacob on 03/05/2025.
+//
+
+import Testing
+@testable import runshaw_buses
+
+struct runshaw_busesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
